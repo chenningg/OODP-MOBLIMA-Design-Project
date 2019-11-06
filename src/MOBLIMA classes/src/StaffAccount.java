bf3d5ba308d84d22bdfda6545aa54f20d4f64004
Shaun__ <35448957+cpshaun@users.noreply.github.com>
2019-11-06 16:18:23 +0800

    Adding StaffAccount

---
+++ src/MOBLIMA classes/src/StaffAccount.java
@@ -0,0 +1,30 @@
+public class StaffAccount {
+    //Attributes
+    private String accountID;
+    private String username;
+    private String password;
+
+
+    //Methods
+    //Getters
+    public String getAccountID() {
+        return accountID;
+    }
+    public String getUsername() {
+        return username;
+    }
+    public String getPassword() {
+        return password;
+    }
+
+    //Setters
+    public void setAccountID(String accountID) {
+        this.accountID = accountID;
+    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
