12dfb346667e58537dc6c1efecafc0c6b98b3891
reubenwong97 <reuben.wong.ys@gmail.com>
2019-11-06 17:41:02 +0800

    Update Movie and Manager

--- src/MovieManager.java
+++ src/MovieManager.java
@@ -1,4 +1,6 @@
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class MovieManager {
     private static MovieManager single_instance = null;
@@ -14,8 +16,25 @@ public class MovieManager {
         return single_instance;
     }
 
-    public void addMovie() {
+    Scanner sc = new Scanner(System.in);
 
+    public void addMovie() {
+        Movie newMovie = new Movie();
+        List<Genre> genreList = new ArrayList<>();
+        System.out.println("Enter movieID: ");
+        newMovie.setMovieID(sc.next());
+        System.out.println("Enter movie title: ");
+        newMovie.setTitle(sc.next());
+        System.out.println("Enter number of genres: ");
+        int numGenres = sc.nextInt();
+        System.out.println("Enter the genres: ");
+        for (int i=0;i<numGenres;i++)
+        {
+            System.out.println("Enter the genre: ");
+            String userGenre = sc.next();
+            genreList.add(Genre.valueOf(userGenre));
+        }
+        newMovie.setGenres(genreList);
     }
 
     public void editMovie() {}
