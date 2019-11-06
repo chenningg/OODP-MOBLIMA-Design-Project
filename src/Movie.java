0a3638c35f9bf827c751f47f171868d78dc47e57
reubenwong97 <reuben.wong.ys@gmail.com>
2019-11-06 16:01:00 +0800

    Update Movie.java
    Added Getters and Setters

--- src/Movie.java
+++ src/Movie.java
@@ -19,5 +19,139 @@ public class Movie {
     private long ticketsSold;
     private double grossProfit;
 
+    public String getTitle() {
+        return title;
+    }
 
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getMovieID() {
+        return movieID;
+    }
+
+    public void setMovieID(String movieID) {
+        this.movieID = movieID;
+    }
+
+    public Genre[] getGenres() {
+        return genres;
+    }
+
+    public void setGenres(Genre[] genres) {
+        this.genres = genres;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public void setDirector(String director) {
+        this.director = director;
+    }
+
+    public String[] getCast() {
+        return cast;
+    }
+
+    public void setCast(String[] cast) {
+        this.cast = cast;
+    }
+
+    public String getSynopsis() {
+        return synopsis;
+    }
+
+    public void setSynopsis(String synopsis) {
+        this.synopsis = synopsis;
+    }
+
+    public MovieRating getMovieRating() {
+        return movieRating;
+    }
+
+    public void setMovieRating(MovieRating movieRating) {
+        this.movieRating = movieRating;
+    }
+
+    public MovieFormat[] getMovieFormats() {
+        return movieFormats;
+    }
+
+    public void setMovieFormats(MovieFormat[] movieFormats) {
+        this.movieFormats = movieFormats;
+    }
+
+    public float getMovieDuration() {
+        return movieDuration;
+    }
+
+    public void setMovieDuration(float movieDuration) {
+        this.movieDuration = movieDuration;
+    }
+
+    public Review[] getMovieReviews() {
+        return movieReviews;
+    }
+
+    public void setMovieReviews(Review[] movieReviews) {
+        this.movieReviews = movieReviews;
+    }
+
+    public float getAverageReviewScore() {
+        return averageReviewScore;
+    }
+
+    public void setAverageReviewScore(float averageReviewScore) {
+        this.averageReviewScore = averageReviewScore;
+    }
+
+    public int getTotalReviewNo() {
+        return totalReviewNo;
+    }
+
+    public void setTotalReviewNo(int totalReviewNo) {
+        this.totalReviewNo = totalReviewNo;
+    }
+
+    public float getTotalReviewScore() {
+        return totalReviewScore;
+    }
+
+    public void setTotalReviewScore(float totalReviewScore) {
+        this.totalReviewScore = totalReviewScore;
+    }
+
+    public ShowingStatus getShowingStatus() {
+        return showingStatus;
+    }
+
+    public void setShowingStatus(ShowingStatus showingStatus) {
+        this.showingStatus = showingStatus;
+    }
+
+    public LocalDateTime getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(LocalDateTime releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public long getTicketsSold() {
+        return ticketsSold;
+    }
+
+    public void setTicketsSold(long ticketsSold) {
+        this.ticketsSold = ticketsSold;
+    }
+
+    public double getGrossProfit() {
+        return grossProfit;
+    }
+
+    public void setGrossProfit(double grossProfit) {
+        this.grossProfit = grossProfit;
+    }
 }
