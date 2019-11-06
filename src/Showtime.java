4554a2074d80af97daa4d8dddda9b1edbb2976f1
Shaun__ <35448957+cpshaun@users.noreply.github.com>
2019-11-06 19:02:43 +0800

    Adding Booking,Showtime and Transaction

--- src/Showtime.java
+++ src/Showtime.java
@@ -7,21 +7,65 @@ public class Showtime{
     private String showtimeID;
     private LocalDate date;
     private LocalTime time;
+    private Movie movie;
+    private Cinema cinema;
+    private Cineplex cineplex;
+    private CinemaStatus cinemaStatus;
 
 
-    //Getters & Setters
+    //Methods
+    public void bookseat(String seatID){
+
+    }
+    public void removeSeatBooking(String seatID){
+
+    }
+    public int getSeatFilledAmount(){
+
+    }
+    //Getters
     public void getShowtimeID(){
         this.showtimeID = showtimeID;
     }
     public LocalDate getDate() {
         return date;
     }
-    public void setDate(LocalDate date) {
-        this.date = date;
+    public LocalTime getTime() {
+        return time;
+    }
+    public Movie getMovie() {
+        return movie;
+    }
+    public Cinema getCinema() {
+        return cinema;
+    }
+    public Cineplex getCineplex() {
+        return cineplex;
+    }
+    public CinemaStatus getCinemaStatus() {
+        return cinemaStatus;
     }
 
-
+    //Setters
     public void setShowtimeID(String showtimeID) {
         this.showtimeID = showtimeID;
     }
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+    public void setTime(LocalTime time) {
+        this.time = time;
+    }
+    public void setMovie(Movie movie) {
+        this.movie = movie;
+    }
+    public void setCinema(Cinema cinema) {
+        this.cinema = cinema;
+    }
+    public void setCineplex(Cineplex cineplex) {
+        this.cineplex = cineplex;
+    }
+
+
+
 }
