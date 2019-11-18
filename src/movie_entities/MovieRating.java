2711a5c8bb7bee09b501015e43492e240b797648
reubenwong97 <reuben.wong.ys@gmail.com>
2019-11-18 23:02:13 +0800

    Added Javadoc

--- src/movie_entities/MovieRating.java
+++ src/movie_entities/MovieRating.java
@@ -1,5 +1,8 @@
 package movie_entities;
 
+/**
+ * Rating of a movie, e.g. G, PG13, etc.
+ */
 public enum MovieRating {
     G ("G"),
     PG ("PG"),
@@ -10,13 +13,26 @@ public enum MovieRating {
 
     private final String name;
 
+    /**
+     * Constructor for MovieRating enum, taking in the string value of the enum and setting it as an attribute.
+     * @param name of the enum.
+     */
     private MovieRating(String name) { this.name = name; }
 
+    /**
+     * For string comparison.
+     * @param otherName String to be compared to.
+     * @return boolean on whether the String value of MovieRating is equals to otherName.
+     */
     public boolean equalsString(String otherName) {
         // (otherName == null) check is not needed because name.equals(null) returns false
         return name.equals(otherName);
     }
 
+    /**
+     *
+     * @return String value of MovieRating for string comparison purposes.
+     */
     public String toString() {
         return this.name;
     }
